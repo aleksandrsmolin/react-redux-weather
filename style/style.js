3d925abc3a35f8cca408c8cf152e2098d91049e9
Oleksandr Smolin <krakensham@gmail.com>
2017-08-18 17:39:36 +0300

    GoogleMaps implementation

---
+++ style/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'td': {
+    'verticalAlign': 'middle !important',
+    'textAlign': 'center !important'
+  },
+  'th': {
+    'verticalAlign': 'middle !important',
+    'textAlign': 'center !important'
+  },
+  'input-group': {
+    'margin': [{ 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 0 }]
+  },
+  'svg': {
+    'height': [{ 'unit': 'px', 'value': 150 }]
+  }
+});
